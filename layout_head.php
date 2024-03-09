902ccf22854b9dd130b3e6fd83dc48722cf5de18
Alexis <ajcodalify@gmail.com>
2024-03-09 11:16:13 +0800

    m

--- layout_head.php
+++ layout_head.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" media="screen" />
     <!-- admin custom CSS -->
-    <link href="<?php echo $home_url . "libs/css/customer.css" ?>" rel="stylesheet" />
+    <link href="<?php echo $home_url . "libs/css/student.css" ?>" rel="stylesheet" />
 </head>
 <body>
     <!-- include the navigation bar -->
